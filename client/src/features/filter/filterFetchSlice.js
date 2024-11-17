855154ad32edc497f70926e289a025238d04bea5
satyanikithdandamudi <dsnd123456@gmail.com>
2024-11-17 15:20:26 +0530

    deployment-update

--- client/src/features/filter/filterFetchSlice.js
+++ client/src/features/filter/filterFetchSlice.js
@@ -3,7 +3,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const fetchedJobs = createAsyncThunk('jobs/fetchedJobs', async(filters) => {
     const queryParams = new URLSearchParams(filters)
-    const response = await fetch(`${process.env.NODE_URI}/jobs?${queryParams}`);
+    const response = await fetch(`${import.meta.env.NODE_URI}/jobs?${queryParams}`);
     const data = await response.json()
     return data;
 })
